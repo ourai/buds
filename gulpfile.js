924d7b916a04f01a96514fd5d32d1f62bc9692f1
Ourai Lin <ourairyu@gmail.com>
2017-04-06 11:57:41 +0800

    Restructure files

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,23 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
-const rename = require("gulp-rename");
 
 const pkg = require("./bower.json");
 
-gulp.task("export-scss", function() {
-  return gulp.src([
-      "./src/_variables.scss"
-    ])
-    .pipe(concat("_exports.scss"))
-    .pipe(gulp.dest("./dist"));
-});
-
 gulp.task("concat-scss", function() {
   return gulp.src([
-      "./src/_variables.scss",
-      "./src/_base.scss"
+      "./scss/_variables.scss",
+      "./scss/_base.scss"
     ])
     .pipe(concat(`_${pkg.name}.scss`))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./scss"));
 });
 
-gulp.task("compile", ["export-scss", "concat-scss"], function() {
-  return gulp.src(`./dist/_${pkg.name}.scss`)
-    .pipe(rename(`${pkg.name}.scss`))
+gulp.task("compile", ["concat-scss"], function() {
+  return gulp.src(`./scss/_${pkg.name}.scss`)
+    .pipe(concat(`${pkg.name}.scss`))
     .pipe(sass({outputStyle: "expanded", noLineComments: true, keepSpecialComments: 0}).on("error", sass.logError))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./css"));
 });
 
 gulp.task("default", ["compile"]);
