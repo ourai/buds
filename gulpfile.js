0ca2a94bff495f4422a6392d64649e8bd97c981d
Ourai Lin <ourairyu@gmail.com>
2017-04-06 11:25:27 +0800

    Export SCSS variables

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,30 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const scssimport = require("gulp-shopify-sass");
+const concat = require("gulp-concat");
 const rename = require("gulp-rename");
 
 const pkg = require("./bower.json");
 
-gulp.task("import-scss", function() {
-  return gulp
-    .src("./src/_exports.scss")
-    .pipe(scssimport())
-    .pipe(rename(`_${pkg.name}.scss`))
+gulp.task("export-scss", function() {
+  return gulp.src([
+      "./src/_variables.scss"
+    ])
+    .pipe(concat("_exports.scss"))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("compile", ["import-scss"], function() {
-  return gulp
-    .src(`./dist/_${pkg.name}.scss`)
+gulp.task("concat-scss", function() {
+  return gulp.src([
+      "./src/_variables.scss",
+      "./src/_base.scss"
+    ])
+    .pipe(concat(`_${pkg.name}.scss`))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("compile", ["export-scss", "concat-scss"], function() {
+  return gulp.src(`./dist/_${pkg.name}.scss`)
     .pipe(rename(`${pkg.name}.scss`))
     .pipe(sass({outputStyle: "expanded", noLineComments: true, keepSpecialComments: 0}).on("error", sass.logError))
     .pipe(gulp.dest("./dist"));
